e27f487a79b98e1de2d7e6f0931850d3b1202cf3
vijaysingh2819 <60483717+vijaysingh2819@users.noreply.github.com>
2021-08-02 03:01:32 +0530

    Update first.java

--- first.java
+++ first.java
@@ -18,8 +18,7 @@ public class first {
 	{
 		System.out.println(getdomain("https://en.wikipedia.org/wiki/Main_Page"));
 		System.out.println(getdomain("http://en.wikipedia.org/wiki/Main_Page"));
-		System.out.println(getdomain("helloworld.com/wiki/Main_Page"));
-		System.out.println(getdomain("hacker.challenge.org/wiki/Main_Page"));
+		
 		
 	}
 
