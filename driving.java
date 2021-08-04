957f05a7677138cffa2bd21203dcb6328f317592
vijaysingh2819 <60483717+vijaysingh2819@users.noreply.github.com>
2021-08-04 21:44:56 +0530

    uncheckedexcp

--- driving.java
+++ driving.java
@@ -1,10 +1,11 @@
-package customexception;
+package customexception; //checkedexception
 
 import java.util.Scanner;
 
 public class driving {
 
 	public static void main(String[] args) throws underage  {
+		//or use try catch 
 		
 		
 		int age;
@@ -12,19 +13,30 @@ public class driving {
 		Scanner sc = new Scanner(System.in);
 		age =sc.nextInt();
 				
-		if(age<18)
+		try 
 		{
+			if(age<18)
+			
 			throw new underage();
+			
+			else 
+				System.out.println("enter further details ");
+			
 		}
-		else {
-			System.out.println("enter further details ");
+		catch(underage e )
+		{
+			System.out.println(e.getMessage() +"enter proper age ");
 		}
+		}
+	
+		
 	}
 
-}
 
-class underage extends Exception 
+
+class underage extends RuntimeException 
 {
+	//try {}use try catch for both bcz in unchecked it will compile the code but will not 
 	underage()
 	{
 		System.out.println("you are under age for licence ");
